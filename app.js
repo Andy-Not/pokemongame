fda150d62c6e8081308d3782fdeaaa8871308e8d
Joandy Alejo <joandyalejogarcia@outlook.com>
2021-03-12 19:07:17 -0500

    removed health var and set health to each individual pokemon

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let health = 100;
 let inGame = true;
 let attack = 20;
 let criticalAttack = 10;
@@ -6,10 +5,10 @@ let criticalAttack = 10;
 const bag = [
     {pokemonName: "pikachu",
         type: "electric",
-    healthStatus: health},
+    healthStatus: 100},
     {pokemonName: "squirtle",
         type: "water",
-        healthStatus: health}
+        healthStatus: 100}
 ]
 
 alert(`pokemon: ${bag[0].pokemonName}  VS  pokemon: ${bag[1].pokemonName}
