78d81af797c238b830609a78cd2b78fe1a7d3099
Joandy Alejo <joandyalejogarcia@outlook.com>
2021-03-12 19:24:10 -0500

    added gameover logic that set the inGame value to false when the pokemon's health is less or equal to 0.

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const bag = [
         type: "water",
         healthStatus: 100}
 ]
-
+while (inGame === true){
+    if (bag[0].healthStatus <= 0 || bag[1].healthStatus <= 0){
+        alert("GAME OVER")
+        inGame = false;
+    }
+}
 alert(`pokemon: ${bag[0].pokemonName}  VS  pokemon: ${bag[1].pokemonName}
 health: ${bag[0].healthStatus}                    health: ${bag[1].healthStatus}
 type: ${bag[0].type}                 type: ${bag[1].type}`)
