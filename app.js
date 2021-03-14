726a5ad9a1d06969a77df2c793ae23bb5c18af51
Joandy Alejo <joandyalejogarcia@outlook.com>
2021-03-13 22:35:11 -0500

    added critical hit inside the while loop so the value changed every time it loops

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let inGame = true;
-let attack = 20;
-let criticalAttack = 10;
+let attack = 25;
+
 
 const bag = [
     {pokemonName: "pikachu",
@@ -11,16 +11,27 @@ const bag = [
         healthStatus: 100}
 ]
 while (inGame === true){
+
+    let criticalAttack = Math.floor(Math.random()*11);
+
     let pikachu = prompt("type attack to attack").toLowerCase();
 
     while(pikachu !== "attack"){
         pikachu = prompt("type attack to attack").toLowerCase();
     }
-    if (pikachu === "attack"){
+
         bag[1].healthStatus -= attack;
+
+    if (criticalAttack > 5){
+        bag[1].healthStatus -= criticalAttack;
+        alert(`you hit a critical attack! for ${criticalAttack} hp`)
     }
 
     alert(`${bag[0].pokemonName} hit ${bag[1].pokemonName}, ${bag[1].pokemonName}'s health is now ${bag[1].healthStatus}`)
+
+    if (bag[1].healthStatus <= 0 || bag[0].healthStatus <= 0){
+        break;
+    }
 
     let squirtle = prompt("type attack to attack").toLowerCase();
 
@@ -28,18 +39,16 @@ while (inGame === true){
         squirtle = prompt("type attack to attack").toLowerCase();
     }
 
-    if (squirtle === "attack"){
         bag[0].healthStatus -= attack;
-    }
 
     alert(`${bag[1].pokemonName} hit ${bag[0].pokemonName}, ${bag[0].pokemonName}'s health is now ${bag[0].healthStatus}`)
 
-    if (bag[0].healthStatus <= 0 || bag[1].healthStatus <= 0){
-        alert("GAME OVER")
-        inGame = false;
+    if (bag[1].healthStatus <= 0 || bag[0].healthStatus <= 0){
+        break;
     }
 }
+alert("GAME OVER")
 alert(`pokemon: ${bag[0].pokemonName}  VS  pokemon: ${bag[1].pokemonName}
 health: ${bag[0].healthStatus}                    health: ${bag[1].healthStatus}
-type: ${bag[0].type}                 type: ${bag[1].type}`)
+type: ${bag[0].type}                type: ${bag[1].type}`)
 
