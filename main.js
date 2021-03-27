d8bf8791ca83c495d59290dcbfa99409e652b35c
Joandy Alejo <joandyalejogarcia@outlook.com>
2021-03-26 21:04:50 -0400

    added logic to reset button still needs work but it works

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function displayFeedback(feedback){
     document.getElementById("feedbackbox").innerHTML = feedback;
 }
+
 function btnDisable(buttonID){
     document.getElementById(buttonID).style.display = "none";
+}
+function btnEnable(buttonID){
+    document.getElementById(buttonID).style.display = "inline-block";
+}
+
+function resetBtn(displaytype){
+    if (isGameOver() === "gameover"){
+        document.getElementById("reset_btn").style.display = "inline-block";
+    }
 }
 
  function isGameOver(){
@@ -17,10 +27,11 @@ function btnDisable(buttonID){
        btnDisable("attack_btn");
        displayFeedback(`GAMEOVER! ${winner.toUpperCase()} WON THE BATTLE`);
        // await syncWait(1500);
-        return false;
+        return "gameover";
     }
 
 }
+
 function getCritical(enemy, num){
 
     let criticalAttack = Math.floor(Math.random()*num);
@@ -67,14 +78,15 @@ async function attack() {
     if (neutralAttack(1) !== "missed") {
         neutralAttack(1);
         getCritical(1, 15);
-        displayFeedback(`you hit ${bag[1].pokemonName}its health is now ${bag[1].healthStatus}`);
+        displayFeedback(`you hit ${bag[1].pokemonName} ${bag[1].pokemonName} health is now ${bag[1].healthStatus}`);
         await syncWait(1500);
     }else {
         displayFeedback("your attacked missed");
         await syncWait(1500);
     }
-    displayFeedback("squirtle is now attacking");
-    await syncWait(1500);
+    //   might add this later but for now is off the game
+    // displayFeedback("squirtle is now attacking");
+    // await syncWait(1500);
     enemyAttack();
     await syncWait(1500);
     displayFeedback("Choose a move");
@@ -82,14 +94,13 @@ async function attack() {
 }
 
 async function enemyAttack(){
-    if(isGameOver() !== false){
+    if (isGameOver() !== "gameover"){
         let autoMove = Math.floor(Math.random()*11);
         if (autoMove >= 4){
             neutralAttack(0);
             getCritical(0, 7);
             displayFeedback(`${bag[1].pokemonName} hit ${bag[0].pokemonName}, ${bag[0].pokemonName}'s health is now ${bag[0].healthStatus}`);
             await syncWait(1500);
-
         }else if(autoMove < 4 && bag[1].healthStatus < bag[1].maxHealth){
             healPokemon(1);
             displayFeedback(`squirtle healed for 10hp squirtle's health is now ${bag[1].healthStatus}`);
@@ -120,9 +131,18 @@ async function heal(){
 
 document.getElementById("attack_btn").addEventListener("click",function () {
     attack();
+    resetBtn();
 });
 
 document.getElementById("heal_btn").addEventListener("click",function () {
     heal();
+    resetBtn();
+});
+document.getElementById("reset_btn").addEventListener("click",function (){
+    bag[1].healthStatus = bag[1].maxHealth;
+    bag[0].healthStatus = bag[0].maxHealth;
+    document.getElementById("reset_btn").style.display = "none";
+    btnEnable("heal_btn");
+    btnEnable("attack_btn");
 });
 
